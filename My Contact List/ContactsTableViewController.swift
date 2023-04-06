5684a48dbd723a82bdce434577b00b1672e232ad
yovany_23 <yovany1061@gmail.com>
2023-04-06 12:13:22 -0400

    Successfully retrieved data from Core Data (Listing 12.2)

--- My Contact List/ContactsTableViewController.swift
+++ My Contact List/ContactsTableViewController.swift
@@ -6,19 +6,42 @@
 //
 
 import UIKit
+import CoreData
 
 class ContactsTableViewController: UITableViewController {
     
-    let contacts = ["Messi", "Neymar", "Ronaldo", "Iniesta", "Xavi", "Pedri", "Gavi"]
+//    let contacts = ["Carl", "Migger", "HH", "And Dee", "Brayan", "Cuzzo"]
+    
+    
+    var contacts: [NSManagedObject] = []
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        loadDataFromDatabase()
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated
+    }
+    
+    func loadDataFromDatabase() {
+        let context = appDelegate.persistentContainer.viewContext
+        let request = NSFetchRequest<NSManagedObject>(entityName: "Contact")
+        
+        do {
+            contacts = try context.fetch(request)
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
     }
 
     // MARK: - Table view data source
@@ -38,8 +61,10 @@ class ContactsTableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ContactsCell", for: indexPath)
 
         // Configure the cell...
-        cell.textLabel?.text = contacts[indexPath.row]
-
+        let contact = contacts[indexPath.row] as? Contact
+        cell.textLabel?.text = contact?.contactName
+        cell.detailTextLabel?.text = contact?.city
+        
         return cell
     }
 

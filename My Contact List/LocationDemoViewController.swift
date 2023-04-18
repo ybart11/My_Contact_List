e2ebc67cc904dbbbf265f30671ae51cdb297512d
yovany_23 <yovany1061@gmail.com>
2023-04-18 19:58:55 -0400

    Handling errors from location manager (Listing 13.7)

--- My Contact List/LocationDemoViewController.swift
+++ My Contact List/LocationDemoViewController.swift
@@ -57,6 +57,53 @@ class LocationDemoViewController: UIViewController, CLLocationManagerDelegate {
         }
     }
     
+    // Whenever Location Manager updates the location of the device
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            let eventDate = location.timestamp
+            let howRecent = eventDate.timeIntervalSinceNow
+            if Double(howRecent) < 15.0 {
+                let coordinate = location.coordinate
+                lblLongitude.text = String(format: "%g\u{00B0}", coordinate.longitude)
+                lblLatitude.text = String(format: "%g\u{00B0}", coordinate.latitude)
+                lblLocationAccuracy.text = String(format: "%gm", location.horizontalAccuracy)
+                lblAltitude.text = String(format: "%gm", location.altitude)
+                lblAltitudeAccuracy.text  = String(format: "%gm", location.verticalAccuracy)
+            }
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
+        if newHeading.headingAccuracy > 0 {
+            let theHeading = newHeading.trueHeading
+            var direction: String
+            switch theHeading {
+            case 225..<315:
+                direction = "W"
+            case 135..<225:
+                direction = "S"
+            case 45..<135:
+                direction = "E"
+            default:
+                direction = "N"
+            }
+            
+            lblHeading.text = String(format: "%g\u{00B0} (%@)", theHeading, direction)
+            lblHeadingAccuracy.text = String(format: "%g\u{00B0}", newHeading.headingAccuracy)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        let errorType = error._code == CLError.denied.rawValue ? "Location Permission Denied" : "Unknown Error"
+        let alertController = UIAlertController(title: "Error Getting Location \(errorType)",
+                                                message: "Error Message: \(error.localizedDescription))",
+                                                preferredStyle: .alert)
+        let actionOk = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        alertController.addAction(actionOk)
+        present(alertController, animated: true, completion: nil)
+    }
+    
     @IBAction func addressToCoordinates(_ sender: Any) {
         let address = "\(txtStreet.text!), \(txtCity.text!), \(txtState.text!)"
         
@@ -87,12 +134,16 @@ class LocationDemoViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     @IBAction func deviceCoordinates(_ sender: Any) {
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.distanceFilter = 100
+        locationManager.startUpdatingLocation()
+        locationManager.startUpdatingHeading()
     }
     
-    
-    
-    
-    
+    override func viewDidDisappear(_ animated: Bool) {
+        locationManager.stopUpdatingLocation()
+        locationManager.stopUpdatingHeading()
+    }
     
     
     

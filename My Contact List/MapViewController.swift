3b41603a7713db86beb1b4836645012720d11378
yovany_23 <yovany1061@gmail.com>
2023-04-18 20:04:25 -0400

    Switched start arrow back to Tab Bar and added Map Kit View on Map Scene

--- My Contact List/MapViewController.swift
+++ My Contact List/MapViewController.swift
@@ -6,8 +6,12 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,15 +19,5 @@ class MapViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
